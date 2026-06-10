62f01e462c5d038e2a80a10030d5af9e16df9826
AmeliaCandra21 <ameliacandradewi21@gmail.com>
2026-06-10 11:57:08 +0700

    sidebar

---
+++ sidebar.js
@@ -0,0 +1,55 @@
+// String 20 
+var laguPopuler = "Kicau Mania";
+var jenisBunga = "Mawar";
+var merekSandal = "Swallow";
+var jenisPayung = "Lipat";
+var bentukMeja = "Persegi Panjang";
+var varianJus = "Jambu";
+var merkKipas = "Miyako";
+var jenisIkan = "Lele";
+var jenisSayur = "Brokoli";
+var namaDanau = "Toba";
+var jenisSerangga = "Capung";
+var merkMotor = "Honda";
+var rasaCiki = "Asin";
+var jenisGunting = "Kain";
+var merekAC = "Sharp";
+var namaPulau = "Bali";
+var jenisKue = "Donat";
+var namaSamudra = "Hindia";
+var merkRiceCooker = "Philips";
+var jenisKayu = "Jati";
+
+// Number 15 
+var lamaPerjalanan = 45;
+var jumlahJendela = 6;
+var panjangKaki = 25;
+var lebarPintu = 80;
+var tinggiLemari = 180;
+var jumlahSendok = 10;
+var kapasitasMesinCuci = 7;
+var ketebalanKardus = 5;
+var suhuSetrika = 150;
+var suhuKulkas = 4;
+var beratSemangka = 4;
+var tinggiPohon = 700;
+var luasKamar = 36;
+var diameterKoin = 2;
+var kedalamanSumur = 8;
+
+// Boolean 15
+var esMencairDipanas = true;
+var plastikMudahSobek = true;
+var kacaBening = true;
+var karetPenghantarListrik = false;
+var emasBerwarnaKuning = true;
+var perakLebihMurahDariEmas = true;
+var tembagaMenghantarkanListrik = true;
+var kayuBisaTerbakar = true;
+var plastikMudahPatah = false;
+var karetGelangElastis = true;
+var sponsMenyerapAir = true;
+var kapasTahanApi = false;
+var besiBisaDitarikMagnet = true;
+var alumuniumRingan = true;
+var airPutihTidakBerwarna = true;
